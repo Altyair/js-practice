5bd320abc8fb33c43291ea3d31baf78806ea3d2f
dzhumaniyazovdm <gitlab@xrm.ru>
2024-06-09 13:44:07 +0300

    rename js, edit webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ module.exports = {
     mode: 'development',
     context: __dirname,
     devtool: "source-map",
-    entry: "./js/3d_figure_tests.js",
+    entry: "./js/script.js",
     output: {
         path: resolve(__dirname, './dist/'),
         filename: './dist/js/main.js',
