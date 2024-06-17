94165fb9d429834acb96e957c0a280856744cc15
dzhumaniyazovdm <gitlab@xrm.ru>
2024-06-17 19:05:26 +0300

    refactoring

--- js/examples/ex1.js
+++ js/examples/ex1.js
@@ -1,62 +1,11 @@
-import { createBlockForCode } from '../helper'
+import { createBlockForCode, setupCanvas, drawCanvas } from '../helper';
 
 // ---------------------------------- canvas
 export const main = () => {
-    const S = Math.sin, C = Math.cos;
-    let w, h, canvas, ctx, t = 0;
-
-    function createCanvas() {
-        canvas = document.createElement('canvas');
-
-        canvas.id = "canvas";
-        canvas.style.zIndex = 8;
-        canvas.style.position = "relative";
-        canvas.style.border = "1px solid";
-        canvas.style.backgroundColor = "black";
-
-        content.appendChild(canvas);
-        ctx = canvas.getContext("2d");
-    }
-
-    function resize() {
-        w = 1200;
-        h = window.innerHeight - 100;
-        canvas.width = w;
-        canvas.height = h;
-    }
-
-    function setup() {
-        createCanvas();
-        window.onresize = resize;
-        resize();
-    }
-
-    function roundUp(num, precision = 2) {
-        precision = Math.pow(10, precision);
-        return Math.ceil(num * precision) / precision;
-    }
-
-    function getRandomNumber(min, max) {
-        return Math.random() * (max - min) + min
-    }
-
-    function clear() {
-        ctx.clearRect(0, 0, w, h);
-        ctx.fillStyle = "black";
-        ctx.fillRect(0, 0, w, h);
-    }
-
-    let pageX
-    function draw2() {
-        clear();
-
+    const { w, h, canvas, context } = setupCanvas(document.querySelector('#content'));
+    drawCanvas({w, h, context}, () => {
         canvas.addEventListener('mousemove', event => {
             console.log(event);
         });
-
-        // requestAnimationFrame(draw2);
-    }
-
-    setup();
-    draw2();
+    });
 }
--- js/helper.js
+++ js/helper.js
@@ -11,3 +11,61 @@ export const createBlockForCode = (code) => {
     `;
     content.appendChild(codeBlock);
 }
+
+export const roundUp = (num, precision = 2) => {
+    precision = Math.pow(10, precision);
+    return Math.ceil(num * precision) / precision;
+}
+
+export const getRandomNumber = (min, max) => {
+    return Math.random() * (max - min) + min
+}
+
+const createCanvas = (parent) => {
+    const canvas = document.createElement('canvas');
+
+    canvas.id = "canvas";
+    canvas.style.zIndex = 8;
+    canvas.style.position = "relative";
+    canvas.style.border = "1px solid";
+    canvas.style.backgroundColor = "black";
+
+    parent.appendChild(canvas);
+    const context = canvas.getContext("2d");
+
+    return { canvas, context };
+}
+
+const resize = (canvas) => {
+    const w = 1200;
+    const h = window.innerHeight - 100;
+    canvas.width = w;
+    canvas.height = h;
+
+    return { w, h };
+}
+
+export const setupCanvas = (parent) => {
+    const { canvas, context } = createCanvas(parent);
+    window.onresize = resize;
+
+    const { w, h } = resize(canvas);
+
+    return Object.assign({ w, h }, { canvas, context });
+}
+
+export const drawCanvas = (params, drawCallback) => {
+    const clear = () => {
+        params.context.clearRect(0, 0, params.w, params.h);
+        params.context.fillStyle = "black";
+        params.context.fillRect(0, 0, params.w, params.h);
+    }
+
+    const draw = () => {
+        clear();
+        drawCallback();
+        requestAnimationFrame(draw);
+    }
+
+    draw();
+}
