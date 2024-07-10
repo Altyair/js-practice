1258446b712fe9718dd11c0dad3d44f1e9885410
dzhumaniyazovdm <gitlab@xrm.ru>
2024-07-11 01:59:16 +0300

    test

--- js/examples/ex11.js
+++ js/examples/ex11.js
@@ -2,6 +2,7 @@ import { Subject } from "rxjs";
 
 // ----------------------------------  -------------------------------
 export const main = () => {
+    // ---------------------------------------- binary search -------------------
     const binarySearch = (arr, k) => {
         let l = 0, r = arr.length - 1;
 
@@ -21,13 +22,13 @@ export const main = () => {
         return -1;
     }
 
-    console.log(binarySearch([-10, -3, 1, 3, 7, 5, 10, 15], 5));
+    console.log('binarySearch', binarySearch([-10, -3, 1, 3, 7, 5, 10, 15], 5));
 
-    // rxjs
+    // ------------------------------------- rxjs ----------------------------
     const obs = new Subject();
     const obj = {
         next: (res) => {
-            console.log(res, 'test');
+            console.log('Subject rxj: ',res);
         },
         error: (err) => {
             console.log(err);
@@ -35,4 +36,109 @@ export const main = () => {
     };
     obs.subscribe(obj)
     obs.next(3);
+
+    // ------------------------------------- find pairs of numbers whose sum is equal to k -----------
+    const findPair1 = (arr, k) => {
+        for (let i = 0; i < arr.length; i ++) {
+            for (let j = i + 1; j < arr.length; j ++) {
+                if (arr[i] + arr[j] === k) {
+                    return [arr[i], arr[j]];
+                }
+            }
+        }
+        return [];
+    }
+
+    const findPair2 = (arr, k) => {
+        const set = new Set();
+        for (let i = 0; i < arr.length; i ++) {
+            const numberToFind = k - arr[i];
+            if (set.has(numberToFind)) {
+                return [arr[i], numberToFind];
+            } else {
+                set.add(arr[i]);
+            }
+        }
+        return [];
+    }
+
+    const findPair3 = (arr, k) => {
+        for (let i = 0; i < arr.length; i ++) {
+            const numberToFind = k - arr[i];
+
+            let l = 0, r = arr.length - 1;
+            while (l < r) {
+                const mid = Math.floor((r - l) / 2) + l;
+                if (numberToFind === arr[mid]) {
+                    return [numberToFind, arr[i]];
+                }
+                (k < mid) ? r = mid: l = mid + 1;
+            }
+        }
+        return [];
+    }
+
+    const findPair4 = (arr, k) => {
+        let l = 0, r = arr.length - 1;
+        while (l < r) {
+            const sum = arr[l] + arr[r];
+            if (sum === k) {
+                return [arr[l], arr[r]];
+            }
+            (sum < k) ? l ++ : r --;
+        }
+        return [];
+    }
+
+    console.log('find pairs of numbers whose sum is equal to k v1', findPair1([1, 3, 5, 7], 12));
+    console.log('find pairs of numbers whose sum is equal to k v2', findPair2([1, 3, 5, 7], 12));
+    console.log('find pairs of numbers whose sum is equal to k v3', findPair3([1, 3, 5, 7, 12, 17], 12));
+    console.log('find pairs of numbers whose sum is equal to k v4', findPair4([1, 3, 5, 7, 12, 17], 12));
+
+    // ------------------------------------- remove duplicates in array of number -----------
+    const removeDuplicates = (arr) => {
+        for (let i = 0; i < arr.length; i++) {
+            if (arr[i] === arr[i - 1]) {
+                arr.splice(i, 1);
+                i--;
+            }
+        }
+        return arr;
+    }
+    console.log('remove duplicates in array of number', removeDuplicates([1, 1, 3, 3, 5, 7, 7, 12, 12, 12, 17]));
+
+    // ------------------------------------- bubble sort  O(n^2) -----------
+    const bubbleSort = (arr) => {
+        for (let i = 0; i < arr.length - 1; i++) {
+            for (let j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]]; // Меняем значения переменных
+                }
+            }
+        }
+        return arr;
+    }
+
+    console.log('bubble sort', bubbleSort([5,2,9,1,5,6])); // [2,5,1,5,6,9], [2,1,5,5,6,9], [1,2,5,5,6,9],
+
+    // ---------------------------------- sorting by choice  O(n^2) -------------
+    const sortingByChoice = (arr) => {
+        for (let i = 0; i < arr.length; i++) {
+            let min = i;
+            for (let j = i; j < arr.length; j++) {
+                if (arr[j] < arr[min]) {
+                    min = j;
+                }
+            }
+            [arr[i], arr[min]] = [arr[min], arr[i]]; // Меняем значения переменных
+        }
+        return arr;
+    }
+
+    console.log('sorting by choice', sortingByChoice([5,2,9,1,5,6]));
+
+    //--------------------------------- Check the validity of the brackets ---------------
+    const checkValidityBrackets = (brackets) => {
+
+    };
 }
